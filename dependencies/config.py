f8342672aed6f20077f02e3f2bf5cc4ace7c33d5
fernandoveragul <valentingullll@gmail.com>
2023-01-30 22:42:35 +0500

    Add functions for work with JSON

--- dependencies/config.py
+++ dependencies/config.py
@@ -1,9 +1,10 @@
 import os
 import platform
 import sys
+import json
 
 from dependencies.project_data import TypeDir
-from dependencies.exceptions import InvalidFolderType
+from dependencies.exceptions import InvalidFolderType, InvalidTestName
 
 
 def get_path_list_files(*, folder_name: str) -> str | bool:
@@ -26,13 +27,13 @@ def get_filtered_files_list(*, folder_name: str) -> list[str]:
     return list((file for file in list_files if file))
 
 
-def path_to_icon():
+def path_to_icon() -> str:
     path: str = os.path.dirname(sys.argv[0])
     sp: str = "/" if platform.system() == "Linux" else "\\"
     return f'{path}{sp}display{sp}origin_files{sp}icon.png'
 
 
-def get_paths_to_files(*, folder_name: str):
+def get_paths_to_files(*, folder_name: str) -> list[str]:
     files: list = get_list_files(folder_name=folder_name)
     default_path = f'{os.path.dirname(sys.argv[0])}{get_path_list_files(folder_name=folder_name)}'
     paths: list[str] = []
@@ -41,3 +42,29 @@ def get_paths_to_files(*, folder_name: str):
         if file:
             paths.append(f'{default_path}{sp}{file}')
     return paths
+
+
+def load_current_test(path_to_test: str) -> dict[str, list]:
+    with open(path_to_test, 'r') as test:
+        current_test: dict = json.loads(test.read())
+    return current_test
+
+
+def save_current_test(path_to_save: str, test: list[dict]) -> None:
+    with open(path_to_save, 'w') as file_test:
+        file_test.write(json.dumps(test, indent=4))
+
+
+def add_question(questions: list, question: dict) -> list[dict]:
+    questions.append(question)
+    return questions
+
+
+def change_current_test(path_to_test: str, new_data: list[dict]) -> None:
+    old_test: dict[str, list] = load_current_test(path_to_test=path_to_test)
+    new_test = old_test.update({"ex": new_data}) if old_test else None
+    with open(path_to_test, 'w') as file_test:
+        if new_test:
+            file_test.write(json.dumps(new_test))
+        else:
+            raise InvalidTestName()
--- dependencies/exceptions.py
+++ dependencies/exceptions.py
@@ -1,4 +1,12 @@
 class InvalidFolderType(Exception):
     """Invalid type folder, valid types: [examples, tests, tutorials]"""
+
     def __repr__(self):
         return "Raised when the entered folder type is invalid"
+
+
+class InvalidTestName(Exception):
+    """Invalid test name"""
+
+    def __repr__(self):
+        return "Raised when the entered test name is invalid"
