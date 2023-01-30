3d8759848ee89c10380a8cc72beae191fb065930
fernandoveragul <valentingullll@gmail.com>
2023-01-30 14:07:53 +0500

    Refactor dinamic create buttons logic

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.QtCore import QUrl
 from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QLayout, QPushButton, QMessageBox
 from display import main_window, test_window
 from dependencies.config import get_filtered_files_list, get_paths_to_files
+from dependencies.exceptions import InvalidFolderType
 
 
 class Test(QWidget, test_window):
@@ -22,35 +23,26 @@ class Application(QMainWindow, main_window):
         self.__window_with_test = None
         self.__enable_special_settings()
 
-        self.__create_tutorials_buttons(self.layTutorial)
-        self.__create_examples_buttons(self.layExample)
+        self.__create_buttons(self.layTutorial, "tutorials")
+        self.__create_buttons(self.layExample, "examples")
         self.btnRunTest.clicked.connect(self.__open_window_with_current_test)
 
-    def __create_tutorials_buttons(self, layout: QLayout):
+    def __create_buttons(self, layout: QLayout, folder_name: str):
         def add_function_to_button(*, index: int):
-            paths: list[str] = get_paths_to_files(folder_name="tutorials")
-            self.vtextTutorialInfo.setUrl(QUrl(f"file://{paths[index]}"))
+            paths: list[str] = get_paths_to_files(folder_name=folder_name)
+            if folder_name == "tutorials":
+                self.vtextTutorialInfo.setUrl(QUrl(f"file://{paths[index]}"))
+            elif folder_name == "examples":
+                self.vtextExampleInfo.setUrl(QUrl(f"file://{paths[index]}"))
+            else:
+                raise InvalidFolderType()
 
-        files: list[str] = get_filtered_files_list(folder_name="tutorials")
+        files: list[str] = get_filtered_files_list(folder_name=folder_name)
         for i, file in enumerate(files):
             chapter, current = file.split(".")[0].split("_")
             _btn_ = QPushButton()
-            _btn_.setObjectName(f"btnTutorial{chapter}_{current}")
+            _btn_.setObjectName(f"btn{folder_name.capitalize()}{chapter}_{current}")
             _btn_.setText(f"ГЛАВА {current}")
-            _btn_.clicked.connect(lambda ch, ind=i: add_function_to_button(index=ind))
-            layout.addWidget(_btn_)
-
-    def __create_examples_buttons(self, layout: QLayout) -> None:
-        def add_function_to_button(*, index: int):
-            paths: list[str] = get_paths_to_files(folder_name="examples")
-            self.vtextExampleInfo.setUrl(QUrl(f"file://{paths[index]}"))
-
-        files: list[str] = get_filtered_files_list(folder_name="examples")
-        for i, file in enumerate(files):
-            chapter, current = file.split(".")[0].split("_")
-            _btn_ = QPushButton()
-            _btn_.setObjectName(f"btnExample{chapter}_{current}")
-            _btn_.setText(f"ЗАДАНИЕ {current}")
             _btn_.clicked.connect(lambda ch, ind=i: add_function_to_button(index=ind))
             layout.addWidget(_btn_)
 
